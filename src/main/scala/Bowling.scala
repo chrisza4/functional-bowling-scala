2dde15fbbae7d42e993f580d41a5bbe197a82603
chrisza4 <chakrit.lj@gmail.com>
2020-12-14 15:56:59 +0700

    Implement Game

--- src/main/scala/Bowling.scala
+++ src/main/scala/Bowling.scala
@@ -1,67 +1,100 @@
 package bowling
 
-case class Frame(first: Int, second: Option[Int])
+sealed trait Frame {
+  def first: Int
+  def second: Option[Int]
+}
 sealed trait ParsedFrame
+
+case class NormalFrame(first: Int, second: Option[Int]) extends Frame
+case class LastFrame(first: Int, second: Option[Int], third: Option[Int])
+    extends Frame
+    with ParsedFrame
+
+// Parsed Frame
 case class StrikeFrame() extends ParsedFrame
 case class SpareFrame(first: Int, second: Int) extends ParsedFrame
 case class ScoreFrame(first: Int, second: Int) extends ParsedFrame
 case class UnfinishedFrame() extends ParsedFrame
 
 object Bowling {
-  def getScore(
+  def getFrameScore(
       frame: Frame,
       nextFrame: Option[Frame] = None,
       nextNextFrames: Option[Frame] = None
   ): Option[Int] = {
-    def parseFrame(frame: Frame): ParsedFrame = {
-      if (frame.first == 10) {
-        return StrikeFrame()
-      }
-      frame.second match {
-        case None => UnfinishedFrame()
-        case Some(second) =>
-          if (frame.first + second == 10) SpareFrame(frame.first, second)
-          else ScoreFrame(frame.first, second)
-      }
-    }
-
-    def getNextRolls(): Array[Int] = {
-      val firstFrame = nextFrame match {
-        case None                             => Array()
-        case Some(Frame(first, None))         => Array(first)
-        case Some(Frame(first, Some(second))) => Array(first, second)
-      }
-      val secondFrame = nextNextFrames match {
-        case None                             => Array()
-        case Some(Frame(first, None))         => Array(first)
-        case Some(Frame(first, Some(second))) => Array(first, second)
-      }
-
-      firstFrame.concat(secondFrame)
-    }
-
     val parsedFrame = parseFrame(frame)
-
+    val nextRolls = getNextRolls(nextFrame, nextNextFrames)
     parsedFrame match {
-      case UnfinishedFrame()         => None
+      case UnfinishedFrame() => None
+      case LastFrame(first, second, third) =>
+        Some(first + second.getOrElse(0) + third.getOrElse(0))
       case ScoreFrame(first, second) => Some(first + second)
-      case SpareFrame(first, second) => {
-        return Some(first + second + getNextRolls()(0))
-      }
-      case StrikeFrame() => {
-        val nextRoll = nextFrame match {
-          case None        => return None
-          case Some(frame) => frame.first
+      case SpareFrame(first, second) => Some(first + second + nextRolls(0))
+      case StrikeFrame()             => Some(10 + nextRolls(0) + nextRolls(1))
+    }
+  }
+
+  def getGameScore(game: Array[Frame]): Int = {
+    game.zipWithIndex.map {
+      case (frame, index) => {
+        val nextFrame = getArrayOrNone(game, index + 1)
+        val nextNextFrame = getArrayOrNone(game, index + 2)
+        getFrameScore(frame, nextFrame, nextNextFrame) match {
+          case None        => 0
+          case Some(score) => score
         }
-        val nextNextRoll = (nextFrame, nextNextFrames) match {
-          case (None, _)                             => return None
-          case (Some(Frame(first, Some(second))), _) => second
-          case (Some(Frame(_, None)), None)          => return None
-          case (Some(Frame(_, None)), Some(Frame(nextNextFrameFirst, _))) =>
-            nextNextFrameFirst
-        }
-        return Some(10 + getNextRolls()(0) + getNextRolls()(1))
       }
+    }.sum
+  }
+
+  private def parseFrame(frame: Frame): ParsedFrame = {
+    if (frame.isInstanceOf[LastFrame]) {
+      return frame.asInstanceOf[LastFrame]
+    }
+    if (frame.first == 10) {
+      return StrikeFrame()
+    }
+    frame.second match {
+      case None => UnfinishedFrame()
+      case Some(second) =>
+        if (frame.first + second == 10) SpareFrame(frame.first, second)
+        else ScoreFrame(frame.first, second)
+    }
+  }
+
+  private def getNextRolls(
+      nextFrame: Option[Frame],
+      nextNextFrames: Option[Frame]
+  ): Array[Int] = {
+    def frameToRolls(frame: Frame): Array[Int] = {
+      frame match {
+        case NormalFrame(first, None)             => Array(first)
+        case NormalFrame(first, Some(second))     => Array(first, second)
+        case LastFrame(first, None, _)            => Array(first)
+        case LastFrame(first, Some(second), None) => Array(first, second)
+        case LastFrame(first, Some(second), Some(third)) =>
+          Array(first, second, third)
+      }
+    }
+
+    val firstFrameRolls = nextFrame match {
+      case None    => Array()
+      case Some(f) => frameToRolls(f)
+    }
+    val secondFrameRolls = nextNextFrames match {
+      case None    => Array()
+      case Some(f) => frameToRolls(f)
+    }
+
+    firstFrameRolls.concat(secondFrameRolls)
+  }
+
+  private def getArrayOrNone[T](arr: Array[T], index: Int): Option[T] = {
+    try {
+      Some(arr(index))
+    } catch {
+      case _: IndexOutOfBoundsException => None
     }
   }
 }
--- src/test/scala/BowlingTest.scala
+++ src/test/scala/BowlingTest.scala
@@ -1,34 +1,96 @@
 import bowling._
 
-class BowlingTest extends org.scalatest.funsuite.AnyFunSuite {
+class BowlingScoreTest extends org.scalatest.funsuite.AnyFunSuite {
   test("Score Frame") {
-    val frame = Frame(3, Some(4))
-    assertResult(Some(7))(Bowling.getScore(frame))
+    val frame = NormalFrame(3, Some(4))
+    assertResult(Some(7))(Bowling.getFrameScore(frame))
   }
 
   test("Spare Frame") {
-    val frame = Frame(3, Some(7))
-    val nextFrame = Frame(5, Some(2))
-    assertResult(Some(15))(Bowling.getScore(frame, Some(nextFrame)))
+    val frame = NormalFrame(3, Some(7))
+    val nextFrame = NormalFrame(5, Some(2))
+    assertResult(Some(15))(Bowling.getFrameScore(frame, Some(nextFrame)))
   }
 
   test("Strike Frame") {
-    val frame = Frame(10, None)
-    val nextFrame = Frame(5, Some(2))
-    val nextNextFrame = Frame(5, Some(2))
+    val frame = NormalFrame(10, None)
+    val nextFrame = NormalFrame(5, Some(2))
+    val nextNextFrame = NormalFrame(5, Some(2))
 
     assertResult(Some(17))(
-      Bowling.getScore(frame, Some(nextFrame), Some(nextNextFrame))
+      Bowling.getFrameScore(frame, Some(nextFrame), Some(nextNextFrame))
     )
   }
 
   test("Consequtive Strike Frame") {
-    val frame = Frame(10, None)
-    val nextFrame = Frame(10, None)
-    val nextNextFrame = Frame(5, Some(2))
+    val frame = NormalFrame(10, None)
+    val nextFrame = NormalFrame(10, None)
+    val nextNextFrame = NormalFrame(5, Some(2))
 
     assertResult(Some(25))(
-      Bowling.getScore(frame, Some(nextFrame), Some(nextNextFrame))
+      Bowling.getFrameScore(frame, Some(nextFrame), Some(nextNextFrame))
     )
+  }
+
+  test("Strike before last frame") {
+    val frame = NormalFrame(10, None)
+    val nextFrame = LastFrame(10, Some(10), Some(8))
+
+    assertResult(Some(30))(
+      Bowling.getFrameScore(frame, Some(nextFrame), None)
+    )
+  }
+
+  test("Last frame with score") {
+    val frame = LastFrame(5, Some(3), None)
+    assertResult(Some(8))(
+      Bowling.getFrameScore(frame, None, None)
+    )
+  }
+
+  test("Last frame with spare") {
+    val frame = LastFrame(5, Some(5), Some(7))
+    assertResult(Some(17))(
+      Bowling.getFrameScore(frame, None, None)
+    )
+  }
+
+  test("Last frame with strike") {
+    val frame = LastFrame(10, Some(10), Some(7))
+    assertResult(Some(27))(
+      Bowling.getFrameScore(frame, None, None)
+    )
+  }
+}
+
+class BowlingGameTest extends org.scalatest.funsuite.AnyFunSuite {
+  test("All strike game") {
+    val game: Array[Frame] =
+      (Array.fill(9) { NormalFrame(10, None) }) :+ LastFrame(
+        10,
+        Some(10),
+        Some(10)
+      )
+    assertResult(300)(Bowling.getGameScore(game))
+  }
+
+  test("All spare game") {
+    val game: Array[Frame] =
+      (Array.fill(9) { NormalFrame(5, Some(5)) }) :+ LastFrame(
+        5,
+        Some(5),
+        Some(5)
+      )
+    assertResult(150)(Bowling.getGameScore(game))
+  }
+
+  test("All score game") {
+    val game: Array[Frame] =
+      (Array.fill(9) { NormalFrame(9, Some(0)) }) :+ LastFrame(
+        9,
+        Some(0),
+        None
+      )
+    assertResult(90)(Bowling.getGameScore(game))
   }
 }
